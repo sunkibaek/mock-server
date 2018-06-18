0de477fa732e7207ce67e1fe92e04c7611bdb101
Sunki Baek <sunki.baek@gmail.com>
2018-06-18 16:07:50 +0900

    Add implementation example using micro

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const hello = () => {
-  console.log("hello world" as string);
+import { send } from "micro";
+
+import users from "./resolvers/users";
+
+const app = (req: { url: string }, res: any) => {
+  let resolver;
+
+  if (req.url.indexOf("/users") > -1) {
+    resolver = users;
+  }
+
+  if (typeof resolver === "function") {
+    send(res, 200, resolver());
+  }
+
+  send(res, 404, { error: "No route found" });
 };
 
-hello();
+export default app;
---
+++ src/resolvers/users.ts
@@ -0,0 +1,10 @@
+const users = () => {
+  return [
+    {
+      id: "random-id",
+      name: "Hello World"
+    }
+  ];
+};
+
+export default users;
